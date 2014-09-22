a03018395fb1ec4e79966a4f03e2e4649c55c162
Francis Giraldeau <francis.giraldeau@gmail.com>
2014-09-22 15:24:08 -0400

    Add experiment registry
    Build and register experiment objects. These experiments will be defined as
    modules.
    
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

---
+++ lttngcluster/experiments.py
@@ -0,0 +1,24 @@
+# Experiment registration, similar to Django model admin registration
+
+class AlreadyRegistered(Exception):
+    pass
+
+class NotRegistered(Exception):
+    pass
+
+class ExperimentRegistry(object):
+
+    def __init__(self):
+        self._registry = {}  # class_name -> exp_class
+
+    def register(self, exp_class):
+        if (exp_class.__name__ in self._registry):
+            raise AlreadyRegistered('The experiment %s is already registered' % exp_class.__name__)
+        self._registry[exp_class.__name__] = exp_class()
+
+    def get_experiment(self, name):
+        if (name not in self._registry):
+            raise NotRegistered('The experiment %s is not registered' % name)
+        return self._registry[name]
+
+registry = ExperimentRegistry()
--- tests/lttngcluster_tests.py
+++ tests/lttngcluster_tests.py
@@ -13,3 +13,28 @@ def test_load_script():
     for case in cases:
         cnt = install.load_script(case)
         assert len(cnt) > 0
+
+def test_register_experiment():
+    from lttngcluster.experiments import registry
+    from lttngcluster.api import TraceExperiment
+    from lttngcluster.experiments import AlreadyRegistered, NotRegistered
+
+    class TestExperiment(TraceExperiment):
+        pass
+
+    registry.register(TestExperiment)
+    exp = registry.get_experiment('TestExperiment')
+    exception1 = None
+    exception2 = None
+    try:
+        registry.register(TestExperiment)
+    except Exception as e:
+        exception1 = e
+    try:
+        registry.get_experiment('bidon')
+    except Exception as e:
+        exception2 = e
+
+    assert isinstance(exp, TraceExperiment)
+    assert isinstance(exception1, AlreadyRegistered)
+    assert isinstance(exception2, NotRegistered)
