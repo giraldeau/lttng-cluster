3a74dcb7df71116ae292b184fc9bfa2135203ab1
Francois Doray <fdoray@chromium.org>
2014-11-30 17:08:46 -0500

    Fixes to make the installation work.
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- lttngcluster/commands/base.py
+++ lttngcluster/commands/base.py
@@ -17,16 +17,20 @@ class BaseCommand(object):
 
 class ActionCommand(object):
     actions = {}
+
     def arguments(self, parser):
+        parser.add_argument('-r', '--remotes', type=str, help='comma-separated list of remote hosts on which to execute the command')
+        parser.add_argument('-u', '--user', type=str, help='user that will execute the command on the remote hosts')
         parser.add_argument('action', choices=self.actions.keys(), help='install action');
         parser.add_argument("script", nargs=argparse.REMAINDER)
 
     def handle(self, args):
-        hosts_list = string.split(args.hosts, ',')
+        print repr(args)
+        remotes_list = string.split(args.remotes, ',')
         exe = self.actions[args.action]
         env.user=args.user
         try:
-            execute(exe, args, hosts=hosts_list)
+            execute(exe, args, hosts=remotes_list)
         except KeyboardInterrupt:
             print("Abort")
         finally:
--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ config = {
     'download_url': 'TBD',
     'version': '0.1dev',
     'install_requires': ['fabric'],
-    'packages': ['lttngcluster'],
+    'packages': ['lttngcluster',
+                 'lttngcluster/commands',
+                 'lttngcluster/experiments'],
     'scripts': ['bin/lttng-cluster'],
 }
 
